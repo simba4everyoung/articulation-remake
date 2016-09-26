17cc0c2c76d59320160b3abbd6013c89f9a598bb
simba4everyoung <simba.xu@soulfx.com>
2016-09-26 17:03:55 -0400

    Update: form post

--- app/shared/search-keyword/searchKeywordController.js
+++ app/shared/search-keyword/searchKeywordController.js
@@ -1,3 +1,15 @@
-app.controller('searchKeywordController', function(){
-  this.keyword = '';
+app.controller('searchKeywordController', function($http){
+  this.keyword = {};
+  
+  this.post = function() {
+    $http({
+      method: 'POST',
+      url: 'http://articulation-dev.soulfxtech.com/api/Outbound/Search?',
+      data: $.param(this.keyword),
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+    })
+    .success(function(data) {
+      console.log(data);
+    });
+  };
 });
--- js/main.js
+++ js/main.js
@@ -35,8 +35,20 @@ app.controller('inboundController', function() {
 app.controller('outboundController', function() {
   this.title = 'Outbound Pathways';
 });
-app.controller('searchKeywordController', function(){
-  this.keyword = '';
+app.controller('searchKeywordController', function($http){
+  this.keyword = {};
+  
+  this.post = function() {
+    $http({
+      method: 'POST',
+      url: 'http://articulation-dev.soulfxtech.com/api/Outbound/Search?',
+      data: $.param(this.keyword),
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+    })
+    .success(function(data) {
+      console.log(data);
+    });
+  };
 });
 app.directive('searchKeyword', function() {
   return {
